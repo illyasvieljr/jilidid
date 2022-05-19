2d7063a67ed68b73b23598906dc5e331468a2b02
illyasvieljr <32120029+illyasvieljr@users.noreply.github.com>
2022-05-19 09:00:35 +0700

    Add files via upload

---
+++ donasi.js
@@ -0,0 +1,4 @@
+function donasi() {
+document.getElementById("donasi").style.fontSize = "x-large";
+document.getElementById("donasi").innerHTML = "<b>TERIMAKASIH</b><br /><marquee width='30%' bgcolor='#ff0000' onmouseover='this.stop()' onmouseout='this.start()'>NEW 🙏 OLD</marquee>";
+}
