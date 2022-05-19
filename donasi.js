b223f660dd476fdf5fc727f7113c23a57d18b88b
illyasvieljr <32120029+illyasvieljr@users.noreply.github.com>
2022-05-19 08:59:12 +0700

    Delete donasi.js

--- donasi.js
+++
@@ -1,4 +0,0 @@
-function donasi() {
-document.getElementById("donasi").style.fontSize = "x-large";
-document.getElementById("donasi").innerHTML = "<b>TERIMAKASIHAAAA</b><br /><marquee width='30%' bgcolor='#ff0000' onmouseover='this.stop()' onmouseout='this.start()'>NEW 🙏 OLD</marquee>";
-}
