c5a2790ce6e57a761801aab8ab0e57bdedf94e6a
illyasvieljr <32120029+illyasvieljr@users.noreply.github.com>
2022-05-19 08:57:29 +0700

    Update donasi.js

--- donasi.js
+++ donasi.js
@@ -1,4 +1,4 @@
 function donasi() {
 document.getElementById("donasi").style.fontSize = "x-large";
-document.getElementById("donasi").innerHTML = "<b>TERIMAKASIH</b><br /><marquee width='30%' bgcolor='#ff0000' onmouseover='this.stop()' onmouseout='this.start()'>NEW | OLD</marquee>";
+document.getElementById("donasi").innerHTML = "<b>TERIMAKASIH</b><br /><marquee width='30%' bgcolor='#ff0000' onmouseover='this.stop()' onmouseout='this.start()'>NEW 🙏 OLD</marquee>";
 }
